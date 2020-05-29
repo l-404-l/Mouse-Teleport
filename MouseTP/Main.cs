a8da4af82f1a027f3b184ba06977be7e347c41bd
l-404-l <saywhattowhat@gmail.com>
2020-05-29 10:53:25 -0500

    Add project files.

---
+++ MouseTP/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseTP
+{
+    public static class BuildInfo
+    {
+        public const string Name = "Mouse Teleport"; // Name of the Mod.
+        public const string Author = "404#0004"; // Author of the Mod.
+        public const string Company = "I am not a company -Kappa-"; // Company that made the Mod.
+        public const string Version = "1.0.0"; // Version of the Mod.
+        public const string DownloadLink = "https://github.com/l-404-l/HWIDBypass/releases"; // Download Link for the Mod.
+    }
+    public class Main
+    {
+    }
+}
